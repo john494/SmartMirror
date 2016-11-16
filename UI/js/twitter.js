7ea4f3d6761781765d85b36e88837d49b2b16bbf
Nicholas Kabalan <ntkabalan@gmail.com>
2016-11-16 12:56:04 -0500

    Pulling from Twitter (not yet displayed)

--- UI/js/twitter.js
+++ UI/js/twitter.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+  function getTweets() {
+    $.get("http://jarvis.cse.buffalo.edu/mine/twitter.php", function(body) {
+      // console.log(body);
+    });
+  }
 
-
+  getTweets();
+  setInterval(getTweets, 20000); // 20 seconds
 });
