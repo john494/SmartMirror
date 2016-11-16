d92faf6ca2c8228efc8dd68fb22484f0abb9afc6
Alex Kirschner <ajkirsch@buffalo.edu>
2016-11-16 16:38:37 -0500

    4 edit text fields and enter button gets all stocks as strings #81

--- phoneapp/SmartMirror/app/src/main/java/com/example/smartmirror/stocks.java
+++ phoneapp/SmartMirror/app/src/main/java/com/example/smartmirror/stocks.java
@@ -2,6 +2,9 @@ package com.example.smartmirror;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class stocks extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class stocks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stocks);
+    }
+
+    public void getStocks(View v){
+        EditText text1;
+        EditText text2;
+        EditText text3;
+        EditText text4;
+        String stock1 = "";
+        String stock2 = "";
+        String stock3 = "";
+        String stock4 = "";
+        text1   = (EditText)findViewById(R.id.stock1);
+        stock1 = text1.getText().toString();
+        text2   = (EditText)findViewById(R.id.stock2);
+        stock2 = text2.getText().toString();
+        text3   = (EditText)findViewById(R.id.stock3);
+        stock3 = text3.getText().toString();
+        text4   = (EditText)findViewById(R.id.stock4);
+        stock4 = text4.getText().toString();
+
+
+
+        Log.i("EditText", stock1);
+        Log.i("EditText", stock2);
+        Log.i("EditText", stock3);
+        Log.i("EditText", stock4);
     }
 }
