cea4ea8abdae5cde5969915f775cd7203e32ad61
Nicholas Kabalan <ntkabalan@gmail.com>
2016-11-16 13:03:29 -0500

    Created twitter.js (to get rid of an error on Mike's end)

---
+++ UI/js/facebook.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  function getTweets() {
+    $.get("http://jarvis.cse.buffalo.edu/mine/facebook.php", function(body) {
+      console.log(body);
+    });
+  }
+
+  getTweets();
+  setInterval(getTweets, 20000); // 20 seconds
+});
