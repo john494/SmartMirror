342154bc8ad28615e3fca0945b8477bad9d9415c
Unknown <ajkirschner16@gmail.com>
2016-11-15 19:41:21 -0500

    Fix facebook api issue. Added popup when zipcode is accepted

--- phoneapp/SmartMirror/app/src/main/java/com/example/smartmirror/zipcode.java
+++ phoneapp/SmartMirror/app/src/main/java/com/example/smartmirror/zipcode.java
@@ -1,9 +1,12 @@
 package com.example.smartmirror;
 
+import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,12 +19,17 @@ public class zipcode extends AppCompatActivity {
         setContentView(R.layout.activity_zipcode);
     }
 
-    public String getZip(View v){
+    public void getZip(View v){
         EditText mEdit;
         mEdit   = (EditText)findViewById(R.id.postal_code);
-        String zip = mEdit.getText().toString().trim();
+        String zip = mEdit.getText().toString();
         Log.i("EditText", zip);
-        return zip;
+        mEdit.setText("");
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+
+        Snackbar mySnackbar = Snackbar.make(v, "Zip code " + zip + " accepted!", Snackbar.LENGTH_LONG);
+        mySnackbar.show();
 
     }
 }
