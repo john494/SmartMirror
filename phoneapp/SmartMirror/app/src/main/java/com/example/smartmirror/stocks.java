081b55603c4c49d06f2d95ec32d32412032a2ab3
Alex Kirschner <ajkirsch@buffalo.edu>
2016-11-19 14:09:30 -0500

    Stocks done and post to server #81. Also fixed 2 app icon bug #89

--- phoneapp/SmartMirror/app/src/main/java/com/example/smartmirror/stocks.java
+++ phoneapp/SmartMirror/app/src/main/java/com/example/smartmirror/stocks.java
@@ -1,10 +1,25 @@
 package com.example.smartmirror;
 
+import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class stocks extends AppCompatActivity {
 
@@ -32,11 +47,38 @@ public class stocks extends AppCompatActivity {
         text4   = (EditText)findViewById(R.id.stock4);
         stock4 = text4.getText().toString();
 
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);                                      //kill the keyboard
+        Snackbar mySnackbar = Snackbar.make(v, "Stock ticker symbols accepted!", Snackbar.LENGTH_LONG);
+        mySnackbar.show();
+        text1.setText("");
+        text2.setText("");
+        text3.setText("");
+        text4.setText("");
 
+        final String server = "http://jarvis.cse.buffalo.edu/mine/stocks.php";
+        Log.d("logged", "Post to server");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(server);
+        try {
+            Log.i("logged", "Attempting to post to: "+server);
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
 
-        Log.i("EditText", stock1);
-        Log.i("EditText", stock2);
-        Log.i("EditText", stock3);
-        Log.i("EditText", stock4);
+            nameValuePairs.add(new BasicNameValuePair("fstock1", stock1));
+            nameValuePairs.add(new BasicNameValuePair("fstock2", stock2));
+            nameValuePairs.add(new BasicNameValuePair("fstock3", stock3));
+            nameValuePairs.add(new BasicNameValuePair("fstock4", stock4));
+            Log.i("logged", "Items: " + nameValuePairs);
+
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+
     }
 }
