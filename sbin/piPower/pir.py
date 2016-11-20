2af8da43230726ca96ae24da507254d295dc6449
John <ajkirschner16@gmail.com>
2016-11-20 17:47:03 -0500

    Fixed screen on/off #94

--- sbin/piPower/pir.py
+++ sbin/piPower/pir.py
@@ -11,19 +11,18 @@ def main():
     count = 0
     while True:
         i=GPIO.input(11)
-        tvstatus=0                             #comment this out
-        #Uncomment this code if tv works to power off. My tests show samsung works
+        #My tests show samsung works
         #insignia does not
-        # if i==0:                 #When output from motion sensor is LOW
-        #     print "No intruders",i
-        #     if tvstatus==1 and count==200:  #tv stays on for about 30 sec. adjust count to change
-        #                                     #adjust count to 200 for 30 second timeout. Higher for more
-        #         turn_off()                  #for insignia tv this does not work. Need to manually turn off. 
-        #                                     #This will work with samsung tv with anynet+
-        #         tvstatus=0
-        #     count=count+1
-        #     time.sleep(0.1)
-        if i==1:               #When output from motion sensor is HIGH
+        if i==0:                 #When output from motion sensor is LOW
+             print "No intruders",i
+             if tvstatus==1 and count==200:  #tv stays on for about 30 sec. adjust count to change
+                                             #adjust count to 200 for 30 second timeout. Higher for more
+                 turn_off()                  #for insignia tv this does not work. Need to manually turn off. 
+                                             #This will work with samsung tv with anynet+
+                 tvstatus=0
+             count=count+1
+             time.sleep(0.1)
+        elif i==1:               #When output from motion sensor is HIGH
             print "Intruder detected",i
             if tvstatus==0:
                 turn_on()
@@ -33,9 +32,11 @@ def main():
 
 def turn_on():
     subprocess.call("sh /usr/local/sbin/SmartMirror/piPower/power.sh on", shell=True)
+    #subprocess.call("sh xset dpms force on", shell=True)
 
 def turn_off():
     subprocess.call("sh /usr/local/sbin/SmartMirror/piPower/power.sh off", shell=True)
+    #subprocess.call("sh xset dpms force off", shell=True)
 
 if __name__ == '__main__':
     try:
