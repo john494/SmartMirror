33ac27c85aaa8e7229b1cab94701c80af7c6fa5f
John McKissick <johnmiv94@stny.rr.com>
2016-12-02 14:10:11 -0500

    new app

--- sbin/smartmirror-linux-armv7l/resources/app/js/theme.js
+++ sbin/smartmirror-linux-armv7l/resources/app/js/theme.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   function getLayout(){
+    console.log('Updating layout')
     $.get("http://jarvis.cse.buffalo.edu/mine/layout", function (choice) {
       window.location.replace(choice.slice(0,-1).toLowerCase() + '.html')
     });
   }
 
-getLayout();
-setInterval(getLayout,3000);
+  getLayout();
+  setInterval(getLayout,3000);
 });
