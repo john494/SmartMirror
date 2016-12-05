24679ac396102a8a5d9ca26c445303de82d64f4a
michaelbacki <mdkulbac@buffalo.edu>
2016-12-05 18:44:07 -0500

    added in a quote, changed some themes, error message on non-valid stock

---
+++ UI/js/quotesL.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  function getQuote() {
+    $.get("http://quotes.rest/qod.json?category=life", function(quote) {
+      document.getElementById('quote').innerHTML = '<i>" ' + quote['contents']['quotes'][0]['quote'] +' "</i>'
+      document.getElementById('author').innerHTML = "<b>- " + quote['contents']['quotes'][0]['author']+'</b>'
+
+    });
+  }
+
+  getQuote();
+  // setInterval(getQuote, 900000);
+  var d, c, ampm;
+  updateQuote();
+  function updateQuote() {
+      d = new Date()
+      mm = d.toString("h:mm:ss")
+      var t = setTimeout(updateQuote, 500)
+      if(mm == "12:00:00"){
+        getQuote();
+      }
+  };
+});
